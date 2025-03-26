8c4a49f69e9f16cd592658f9796a82b21b101a6d
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-27 01:38:45 +0330

    edit install script file

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "Start installing Dummy-DNS package"
+printf "Start installing Dummy-DNS package\n"
 
+# shellcheck disable=SC1091
+source .venv/bin/activate
+
+printf "Biulding script...\n"
+pyinstaller --onefile main.py
+
+printf "Copying files...\n" 
+sudo cp dist/main /usr/local/bin/dummy-dns
+sudo chmod +x usr/local/bin/dummy-dns
+
+printf "Save default config into /etc/dummy-dns/deafult.conf\n"
+sudo cp /etc/resolv.conf /etc/dummy-dns/default.conf
+
+printf "Installation is completed.\n"
