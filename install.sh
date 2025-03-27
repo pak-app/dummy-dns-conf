1db4b80879a431044ff2ee70328577c9675f7e68
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-28 01:30:53 +0330

    fix bugs in install script

--- install.sh
+++ install.sh
@@ -14,10 +14,15 @@ printf "Biulding script ...\n"
 pyinstaller --onefile main.py
 
 printf "Copying files...\n" 
-sudo cp dist/main /usr/local/bin/dummy-dns
-sudo chmod +x usr/local/bin/dummy-dns
+sudo cp -v -f ./dist/main /usr/local/bin/dummy-dns
+sudo chmod +x /usr/local/bin/dummy-dns
 
 printf "Save default config into /etc/dummy-dns/deafult.conf\n"
+# Check if /etc/dummy-dns exists or not
+if [ ! -d "/etc/dummy-dns" ]; then
+    sudo mkdir /etc/dummy-dns
+fi
+
 sudo cp /etc/resolv.conf /etc/dummy-dns/default.conf
 
 printf "Installation is completed.\n"
