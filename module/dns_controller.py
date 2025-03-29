8eea35f3fb08b836c6cfe849f1b82f99f6243fab
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-29 05:54:08 +0330

    add exception handler

--- module/dns_controller.py
+++ module/dns_controller.py
@@ -1,27 +1,60 @@
 from .dns_handler import DnsHandler
+import logging
+import os
+from exception_handlers import handle_exceptions
 
+logger = logging.getLogger(os.getenv('APP_LOG_NAME'))
 class DnsController:
     
     def __init__(self, args):
         
         self.args = args
         self.dns_handler = DnsHandler()
-            
+    
+    # Reset DNS configuration
+    @handle_exceptions(
+        successful_message='System DNS is reseted.',
+        error_message='Reset system DNS configuration is failed.'
+    )
     def reset_dns(self):
         self.dns_handler.reset_dns()
-    
+        
+    # Save current system DNS configuration for dummy-dns
+    @handle_exceptions(
+        successful_message='Current system DNS configuration is saved as dummy-dns default system DNS configuration.\nIt is located on /etc/dummy-dns/default.conf',
+        error_message='Save current system configuration for dummy-dns.'
+    )       
     def force_save_conf(self):
         self.dns_handler.force_save_conf()
-    
+
+    # Set given configuration.     
+    @handle_exceptions(
+        successful_message='DNS is set successfully.',
+        error_message='Faild to set your given DNS. Please check the error and try again.'
+    )
     def set_custom_conf(self, config_path: str):
         self.dns_handler.set_custom_conf(config_path)
     
+    # Set default DNS configuration on your machine.(Shecan)
+    @handle_exceptions(
+        successful_message='Default DNS configuration is set successfully.',
+        error_message='Faild to set defualt DNS configuration. Please check the error and try again.'
+    )
     def set_default_conf(self):
         self.dns_handler.set_default_conf()
     
+    # Checking if dummy-dns is set DNS or not
+    @handle_exceptions(
+        error_message='Faild to check dummy-dns status. Please check the error and try again.\nIt may /etc/resolv.conf file damaged. Please use dummy-dns --reset to reset configuration and set DNS again.'
+    )
     def check_dummy(self):
         self.dns_handler.check_dummy()
     
+    # Select DNS configuration form /etc/dummy-dns/config.json by its name
+    @handle_exceptions(
+        successful_message='{name} DNS configuration is set successfully.',
+        error_message='Faild to set {name} DNS configuration. Please check the error and try again.'
+    )
     def select_saved_server(self, name: str) -> None:
         self.dns_handler.select_saved_server(name)
 
